13c60cc17455a25ad339b525f5dd1db63be5cb2d
Edvinas <edvinas.kavaliauskas@jkm.lt>
2018-11-14 11:30:26 +0200

    mini

--- serveris/serveris/Models/User.cs
+++ serveris/serveris/Models/User.cs
@@ -12,6 +12,10 @@ namespace serveris.Models
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public int Role { get; set; } // 1 - Admin, 2 - normal user
+        public double AccountBalance { get; set; }
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
         public string Token { get; set; }
     }
 }
